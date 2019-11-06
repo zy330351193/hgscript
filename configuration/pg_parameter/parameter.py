1776c14452a46ef884d6b133a7682078f772658a
zhangji <zhangji@highgo.com>
2019-11-06 09:06:50 +0800

    完善参数规则

--- configuration/pg_parameter/parameter.py
+++ configuration/pg_parameter/parameter.py
@@ -1,9 +1,33 @@
+# !usr/bin/env python
+# coding=utf-8
+#此参数为安装编译pg和pgpool的参数，若只有1个IP，则其余键值对的值可填'',若不安装pgpool,则pgpoolpath和local_package_path1值为''
+# example：
+# setup= {
+#     'pgusername': 'postgres',
+#     'postgrespasswd':'123456',
+#     'root_password': 'hg192168',
+#     'remote_ip_0':'192.168.222.141',
+#     'remote_ip_1':'192.168.222.142',
+#     'remote_ip_2':'',
+#     'pgpath': '/opt/PG-10.10',
+#     'local_package_path0':'E:\materials\study_materials\package\postgresql-10.10.tar.gz',
+#     'pgpoolpath':'/opt/pgpool-406',
+#     'local_package_path1':'E:\materials\study_materials\package\pgpool-II-4.0.6.tar.gz'
+#
+# }
+
 setup= {
     'pgusername': 'postgres',
-    'pgpasswd':'123456',
-    'root_password': 'z1990712',
-    'remote_ip':'192.168.222.141',
+    'postgrespasswd':'123456',
+    'root_password': 'hg192168',
+    #若安装少于3个IP，则多余的值填''
+    'remote_ip_0':'192.168.222.135',
+    'remote_ip_1':'192.168.222.136',
+    'remote_ip_2':'192.168.222.137',
     'pgpath': '/opt/PG-10.10',
-    'local_package_path':'E:\materials\study_materials\package\postgresql-10.10.tar.gz',
+    'local_package_path0':'E:\materials\study_materials\package\postgresql-10.10.tar.gz',
+    #若不选安装pgpool,则pgpoolpath，local_package_path1值为''.
+    'pgpoolpath':'/opt/pgpool-406',
+    'local_package_path1':'E:\materials\study_materials\package\pgpool-II-4.0.6.tar.gz'
 
 }
