4d503f4f255ae4ad676d128dc2ff7b9d739230ad
zhangji <zhangji@highgo.com>
2019-11-11 15:28:32 +0800

    配置参数

--- configuration/pg_parameter/parameter.py
+++ configuration/pg_parameter/parameter.py
@@ -19,15 +19,15 @@
 setup= {
     'pgusername': 'postgres',
     'postgrespasswd':'123456',
-    'root_password': 'hg192168',
+    'root_password': 'z1990712',
     #若安装少于3个IP，则多余的值填''
-    'remote_ip_0':'192.168.222.135',
-    'remote_ip_1':'192.168.222.136',
-    'remote_ip_2':'192.168.222.137',
+    'remote_ip_0':'192.168.222.141',
+    'remote_ip_1':'',
+    'remote_ip_2':'',
     'pgpath': '/opt/PG-10.10',
     'local_package_path0':'E:\materials\study_materials\package\postgresql-10.10.tar.gz',
     #若不选安装pgpool,则pgpoolpath，local_package_path1值为''.
-    'pgpoolpath':'/opt/pgpool-406',
-    'local_package_path1':'E:\materials\study_materials\package\pgpool-II-4.0.6.tar.gz'
+    'pgpoolpath':'',
+    'local_package_path1':''
 
 }
